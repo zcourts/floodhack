ad3e47593f421b1e389ce1688e113704e25b436a
Richard Caudle <richard.caudle@datasift.com>
2014-02-16 16:40:26 +0000

    Added voice handler

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var twilio = require('twilio'),
     express = require('express'),
-    sms = require('./lib/sms-channel-handler');
+    sms = require('./lib/sms-channel-handler'),
+    voice = sms = require('./lib/voice-channel-handler');
     
 // Create an express web app
 var app = express();
@@ -16,6 +17,50 @@ app.post('/sms', function(request, response) {
     // Render the TwiML response as XML
     response.type('text/xml');
     response.send(twiml.toString());
+
+});
+
+// Create a webhook that handles an incoming voice
+app.post('/voice', function(request, response) {
+
+	var twiml = voice.respond(request.body.Body);
+    
+    // Render the TwiML response as XML
+    response.type('text/xml');
+    response.send(twiml.toString());
+});
+
+// Create a webhook that handles an incoming voice
+app.post('/callstage1', function(request, response) {
+
+	var twiml = voice.stage1(request.body.Digits);
+	
+	// Render the TwiML response as XML
+    response.type('text/xml');
+    response.send(twiml.toString());
+
+});
+
+// Create a webhook that handles an incoming voice
+app.post('/callstage2', function(request, response) {
+
+	var twiml = voice.stage2(request.body.Digits);
+	
+	// Render the TwiML response as XML
+    response.type('text/xml');
+    response.send(twiml.toString());
+
+});
+
+// Create a webhook that handles an incoming voice
+app.post('/callstage3', function(request, response) {
+
+	var twiml = voice.stage3(request.body.Digits);
+	
+	// Render the TwiML response as XML
+    response.type('text/xml');
+    response.send(twiml.toString());
+
 });
  
 // Have express create an HTTP server that will listen on port 3000
